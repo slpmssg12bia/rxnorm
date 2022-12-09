6d979b49cb7844336632c54a97cd745e7d9deb44
slpmssg12bia <102541001+slpmssg12bia@users.noreply.github.com>
2022-12-09 13:32:14 -0500

    Update rxnorm_cron.py

--- rxnorm_cron.py
+++ rxnorm_cron.py
@@ -20,14 +20,15 @@ def get_urls(soup):
 def download_and_extract(urls):
     for texts in urls:
         text = str(texts)
-        file = text[50:142]
+        file = text[54:74]
         print('zip file :', file)
         zip_link = texts['href']
         print('Downloading %s :' %zip_link)
         slashurl = zip_link.split('/')
         print(slashurl)
-        wget.download("https://www.nlm.nih.gov/research/umls/rxnorm/docs/"+ slashurl[1])
-        print("file downloaded....!!!")
+        link = "https://download.nlm.nih.gov/umls/kss/rxnorm/"+ slashurl[1] + "&apiKey=26f0779c-75cc-4611-a9f3-2e0c36f5a17d"
+        subprocess.run(["curl",link,"-o", slashurl[len(slashurl)-1]])
+        print("file downloading...")
         subprocess.run(["mv", slashurl[1], "db.zip"])
         subprocess.run(["unzip", "db.zip"])
         print("uploading the latest dump to s3")
@@ -35,7 +36,7 @@ def download_and_extract(urls):
         subprocess.run(["bash", "/home/ubuntu/rxnorm/rxnorm_clean.sh"])
         return
 
-r = requests.get('https://www.nlm.nih.gov/research/umls/rxnorm/docs/rxnormfiles.html', auth=('sherripytorch@gmail.com' , 'slpbia12#'))
+r = requests.get('https://www.nlm.nih.gov/research/umls/rxnorm/docs/rxnormfiles.html?_gl=1')
 soup = BeautifulSoup(r.content, 'html.parser')
 urls = get_urls(soup)
 download_and_extract(urls)
