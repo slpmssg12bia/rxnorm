0ceb0da8da7c04d8e35ba21c94e31484a1b2c432
slpmssg12bia <102541001+slpmssg12bia@users.noreply.github.com>
2022-12-18 15:57:22 -0500

    Update rxnorm_cron.py

--- rxnorm_cron.py
+++ rxnorm_cron.py
@@ -5,29 +5,29 @@ import re
 from bs4 import BeautifulSoup
 import requests
 import wget
+import zipfile, requests
+from io import BytesIO
 
-def get_urls(soup):
-    urls = []
-    for a in soup.find_all('a', href=True):
-        ul = a.find_all(text=re.compile('RxNorm_full_'))
-        if ul != []:
-            urls.append(a)
-            break
-    print('done scraping the url...!')
-    return urls
+def get_url(soup):
+    for a in soup.select("a[href]"):
+        if a.text.startswith('RxNorm_full_'):
+            return a["href"]
+    print("Error getting url")
+    exit()
 
-def download_and_extract(urls):
-    for texts in urls:
-        text = str(texts)
-        file = text[54:74]
-        print('zip file :', file)
-        zip_link = texts['href']
-        print('Downloading %s :' %zip_link)
-        slashurl = zip_link.split('/')
-        print(slashurl)
-        link = "https://download.nlm.nih.gov/umls/kss/rxnorm/"+ slashurl[1] + "&apiKey=26f0779c-75cc-4611-a9f3-2e0c36f5a17d"
-        subprocess.run(["curl",link,"-o", slashurl[len(slashurl)-1]])
-        print("file downloading...")
+def download_and_extract(url):
+    file_name = url.split('/')[-1]
+    link = f"https://uts-ws.nlm.nih.gov/download?url={url}&apiKey=26f0779c-75cc-4611-a9f3-2e0c36f5a17d"
+    content = BytesIO()
+    print(f'Downloading {file_name}')
+    with requests.get(link, stream=True) as r:
+        for chunk in r.iter_content(chunk_size=1024):
+            content.write(chunk)
+    print("Download Completed")    
+    content.seek(0)
+    zip_file = zipfile.ZipFile(content)
+    zip_file.extractall(file_name)
+        
         subprocess.run(["mv", slashurl[1], "db.zip"])
         subprocess.run(["unzip", "db.zip"])
         print("uploading the latest dump to s3")
@@ -36,6 +36,6 @@ def download_and_extract(urls):
         return
 
 r = requests.get('https://www.nlm.nih.gov/research/umls/rxnorm/docs/rxnormfiles.html?_gl=1')
-soup = BeautifulSoup(r.content, 'html.parser')
-urls = get_urls(soup)
-download_and_extract(urls)
+soup = BeautifulSoup(r.text, 'html.parser')
+url = get_url(soup)
+download_and_extract(url)
