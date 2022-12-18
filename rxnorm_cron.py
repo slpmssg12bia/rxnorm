9ddc6fa1a2bcd78f62c5ec71d22100ab051adbc0
slpmssg12bia <102541001+slpmssg12bia@users.noreply.github.com>
2022-12-18 16:28:38 -0500

    Update rxnorm_cron.py

--- rxnorm_cron.py
+++ rxnorm_cron.py
@@ -8,6 +8,8 @@ import wget
 import zipfile, requests
 from io import BytesIO
 
+dump_folder = "rxnormdump"
+
 def get_url(soup):
     for a in soup.select("a[href]"):
         if a.text.startswith('RxNorm_full_'):
@@ -20,17 +22,23 @@ def download_and_extract(url):
     link = f"https://uts-ws.nlm.nih.gov/download?url={url}&apiKey=26f0779c-75cc-4611-a9f3-2e0c36f5a17d"
     content = BytesIO()
     print(f'Downloading {file_name}')
+    downloaded = 0
+    start = time.time()
     with requests.get(link, stream=True) as r:
-        for chunk in r.iter_content(chunk_size=1024):
+        for chunk in r.iter_content(chunk_size=8192):
+            downloaded += len(chunk)
+            print(f"Downloaded {downloaded / 1024 / 1024:.2f} MB", end="\r", flush=True)
             content.write(chunk)
-    print("Download Completed")    
+    print()
+    end = time.time()
+    print("Download Completed in {:.2f} seconds".format(end - start))    
+
     content.seek(0)
     zip_file = zipfile.ZipFile(content)
-    zip_file.extractall(file_name)
-        
-        subprocess.run(["bash", "/home/ubuntu/rxnorm/rxnorm_dump_to_s3.sh"])
-        subprocess.run(["bash", "/home/ubuntu/rxnorm/rxnorm_clean.sh"])
-        return
+    zip_file.extractall(dump_folder)
+    subprocess.run(["bash", "/home/ubuntu/rxnorm/rxnorm_dump_to_s3.sh"])
+    subprocess.run(["bash", "/home/ubuntu/rxnorm/rxnorm_clean.sh"])
+
 
 r = requests.get('https://www.nlm.nih.gov/research/umls/rxnorm/docs/rxnormfiles.html?_gl=1')
 soup = BeautifulSoup(r.text, 'html.parser')
