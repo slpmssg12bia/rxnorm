8484ef4e53d56b75158d02d29de9ceac02921a67
slpmssg12bia <102541001+slpmssg12bia@users.noreply.github.com>
2022-12-18 15:48:46 -0500

    Update dump_to_s3.sh

--- dump_to_s3.sh
+++ dump_to_s3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir rxnormdump
-mv * rxnormdump
+mv !!!!!!!!!I NEED EVERYTHING IN THE DOWNLOADED ZIPFOLDER TO GO INTO THIS DIRECTORY!!!!! rxnormdump
 aws s3 sync rxnormdump/ s3://viquity-database-import-us-east-1/Jobs/rxnorm/rxnormdump-"$(date +%d-%m-%y-%H-%M)"/
