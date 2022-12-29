b1d81e0d87a2a8e59082d9a781ba01b22460a85d
slpmssg12bia <102541001+slpmssg12bia@users.noreply.github.com>
2022-12-29 10:49:44 -0500

    Update dump_to_s3.sh

--- dump_to_s3.sh
+++ dump_to_s3.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws s3 sync rxnormdump/ s3://viquity-database-import-us-east-1/Jobs/rxnorm/rxnormdump-"$(date +%d-%m-%y-%H-%M)"/
+aws s3 sync rxnormdump/ s3://viquity-database-import-us-east-1/Jobs/rxnorm/rxnorm_current_dump/rxnormdump/
